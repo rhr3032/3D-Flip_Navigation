6b29e72655146a36355261d2d4e8665e1a8598c1
Raisul Rafi <raisulhasanrafi3032@gmail.com>
2021-10-01 05:04:13 +0600

    It's my first commit in this project

---
+++ script.js
@@ -0,0 +1,5 @@
+/*
+    This is External JavaScript
+*/
+
+const clc = document.querySelector('.clc-btn');const hum = document.querySelector('.hum-btn');const navbar = document.querySelector('.navbar');const main = document.querySelector('.main');const a = document.querySelectorAll('a');hum.addEventListener("click", ()=> {navbar.classList.add('active_navbar');main.classList.add('active_main');});clc.addEventListener("click", ()=> {navbar.classList.remove('active_navbar');main.classList.remove('active_main');});a.forEach(function(e){e.addEventListener("click", ()=>{navbar.classList.remove('active_navbar');main.classList.remove('active_main');})});
